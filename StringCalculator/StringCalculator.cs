94219d1ff800e5f68101195bc0cf2e596710fb7c
unknown <krokodyliad@gmail.com>
2022-03-07 09:56:40 +0100

    Bug 2

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -13,7 +13,7 @@ namespace StringCalculator
         public static int CalculateString(string s)
         {
             if (string.IsNullOrWhiteSpace(s))
-                return 0;
+sadiidasjdhasjdhajshbdhjah                return 1213;
 
 
 
